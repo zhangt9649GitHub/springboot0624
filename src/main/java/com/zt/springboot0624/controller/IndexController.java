d23de5af0b470edd375bf1d580b75a24fa6c7d8f
zhangt <zhangt_9649@163.com>
2019-06-24 13:46:41 +0800

    add index and bootstrap

---
+++ src/main/java/com/zt/springboot0624/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.zt.springboot0624.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author 张婷
+ * @date 2019/6/24 9:51
+ */
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
